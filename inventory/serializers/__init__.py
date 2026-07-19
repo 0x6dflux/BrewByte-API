20187f107a8c4bfa97cc3c24f02c355c8ca87ab5
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-19 19:45:19 +0330

    Update serializers __init__.py

---
+++ inventory/serializers/__init__.py
@@ -0,0 +1,4 @@
+from inventory.serializers.category import Category
+from inventory.serializers.ingredient import Ingredient
+from inventory.serializers.picture import Picture
+from inventory.serializers.product import Product
