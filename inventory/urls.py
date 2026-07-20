20a440d0bac010b1c3fcc7565954ba12205c5557
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-20 22:22:42 +0330

    Define urls

---
+++ inventory/urls.py
@@ -0,0 +1,44 @@
+from django.urls import path
+
+from inventory.views import (
+    CategoryListCreateAPIView,
+    CategoryRetrieveUpdateDestroyAPIView,
+    IngredientListCreateAPIView,
+    IngredientRetrieveUpdateDestroyAPIView,
+    PictureListCreateAPIView,
+    PictureRetrieveUpdateDestroyAPIView,
+    ProductListCreateAPIView,
+    ProductRetrieveUpdateDestroyAPIView,
+)
+
+app_name = "inventory"
+urlpatterns = [
+    path("category/", CategoryListCreateAPIView.as_view(), name="category"),
+    path(
+        "category/<int:pk>/",
+        CategoryRetrieveUpdateDestroyAPIView.as_view(),
+        name="category-detail",
+    ),
+    path("ingredient/", IngredientListCreateAPIView.as_view(), name="ingredient"),
+    path(
+        "ingredient/<int:pk>",
+        IngredientRetrieveUpdateDestroyAPIView.as_view(),
+        name="ingredient-detail",
+    ),
+    path("product/", ProductListCreateAPIView.as_view(), name="product"),
+    path(
+        "product/<int:pk>",
+        ProductRetrieveUpdateDestroyAPIView.as_view(),
+        name="product-detail",
+    ),
+    path(
+        "product-picture/",
+        PictureListCreateAPIView.as_view(),
+        name="product-picture",
+    ),
+    path(
+        "product-picture/<int:pk>",
+        PictureRetrieveUpdateDestroyAPIView.as_view(),
+        name="product-picture-detail",
+    ),
+]
