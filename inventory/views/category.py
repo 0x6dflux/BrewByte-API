9dd511cd303c2d54f12918564aa09173f86272c6
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-21 11:19:23 +0330

    [doc] Implement CRUD using generics
    This commit is just an example for using generic views as a reference in
    future.

--- inventory/views/category.py
+++ inventory/views/category.py
@@ -1,46 +1,14 @@
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-from config.mixin import DetailMixin
 from inventory.models import Category
 from inventory.serializers import CategorySerializer
 
 
-class CategoryListCreateAPIView(APIView):
-    def get(self, request: Request) -> Response:
-        s = CategorySerializer(Category.objects.all(), many=True)
-
-        return Response(s.data)
-
-    def post(self, request: Request) -> Response:
-        s = CategorySerializer(data=request.data)
-
-        if s.is_valid():
-            s.save()
-
-            return Response(s.data, HTTP_201_CREATED)
-
-        return Response(s.errors, HTTP_400_BAD_REQUEST)
-
-
-class CategoryRetrieveUpdateDestroyAPIView(DetailMixin, APIView):
-    model_class = Category
+class CategoryListCreateAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    def get(self, request: Request, pk: int) -> Response:
-        s = CategorySerializer(self._get_object_or_404(pk))
 
-        return Response(s.data)
-
-    def put(self, request: Request, pk: int) -> Response:
-        return self._update_product(pk)
-
-    def patch(self, request: Request, pk: int) -> Response:
-        return self._update_product(pk, partial=True)
-
-    def delete(self, request: Request, pk: int) -> Response:
-        self._get_object_or_404(pk).delete()
-
-        return Response()
+class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
