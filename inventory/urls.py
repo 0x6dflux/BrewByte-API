4b4e639af9dc8517396fbd324a28df63506feff3
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-21 16:57:16 +0330

    Redefine urls using SimpleRouter

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,4 @@
-# type: ignore
-
-from django.urls import path
+from rest_framework.routers import SimpleRouter
 
 from inventory.views import (
     CategoryViewSet,
@@ -9,54 +7,11 @@ from inventory.views import (
     ProductViewSet,
 )
 
-action_without_detail = {"get": "list", "post": "create"}
-action_with_detail = {
-    "get": "retrieve",
-    "put": "update",
-    "patch": "partial_update",
-    "delete": "destroy",
-}
+router = SimpleRouter(use_regex_path=False)
+router.register("category", CategoryViewSet, "category")
+router.register("ingredient", IngredientViewSet, "ingredient")
+router.register("product-picture", PictureViewSet, "picture")
+router.register("product", ProductViewSet, "product")
 
 app_name = "inventory"
-urlpatterns = [
-    path(
-        "category/",
-        CategoryViewSet.as_view(action_without_detail),
-        name="category",
-    ),
-    path(
-        "category/<int:pk>/",
-        CategoryViewSet.as_view(action_with_detail),
-        name="category-detail",
-    ),
-    path(
-        "ingredient/",
-        IngredientViewSet.as_view(action_without_detail),
-        name="ingredient",
-    ),
-    path(
-        "ingredient/<int:pk>/",
-        IngredientViewSet.as_view(action_with_detail),
-        name="ingredient-detail",
-    ),
-    path(
-        "product/",
-        ProductViewSet.as_view(action_without_detail),
-        name="product",
-    ),
-    path(
-        "product/<int:pk>/",
-        ProductViewSet.as_view(action_with_detail),
-        name="product-detail",
-    ),
-    path(
-        "product-picture/",
-        PictureViewSet.as_view(action_without_detail),
-        name="product-picture",
-    ),
-    path(
-        "product-picture/<int:pk>/",
-        PictureViewSet.as_view(action_with_detail),
-        name="product-picture-detail",
-    ),
-]
+urlpatterns = router.urls
