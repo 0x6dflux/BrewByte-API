a69d75297d33e1fe06c5ce146650b68513adfbc0
MahdiMohammadiME <mohammadi.mahdi.me@gmail.com>
2026-07-21 16:39:37 +0330

    [doc] Implement CRUD using ModelViewSet
    This commit is just an example for using ModelViewSet as a reference in
    future.

--- inventory/views/category.py
+++ inventory/views/category.py
@@ -1,30 +1,9 @@
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
-from config.mixin import GeneralMixin
 from inventory.models import Category
 from inventory.serializers import CategorySerializer
 
 
-class CategoryViewSet(GeneralMixin, ViewSet):
-    query_set = Category.objects.all()
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-    def list(self, request: Request) -> Response:
-        return self._list()
-
-    def create(self, request: Request) -> Response:
-        return self._create()
-
-    def retrieve(self, request: Request, pk: int) -> Response:
-        return self._retrieve(pk)
-
-    def update(self, request: Request, pk: int) -> Response:
-        return self._update(pk)
-
-    def partial_update(self, request: Request, pk: int) -> Response:
-        return self._update(pk, partial=True)
-
-    def destroy(self, request: Request, pk: int) -> Response:
-        return self._destroy(pk)
